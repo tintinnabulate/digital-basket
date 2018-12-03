4b5873eff54237dc673b151412135c3c33322e94
Tintinnabulate <tintinn@abulate>
2018-12-03 13:49:13 +0000

    Get tests passing

--- basket_test.go
+++ basket_test.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	c "github.com/smartystreets/goconvey/convey"
@@ -36,10 +39,10 @@ func TestGetHomePage(t *testing.T) {
 
 		c.So(err, c.ShouldBeNil)
 
-		c.Convey("The next page body should contain \"I am an alcoholic\"", func() {
+		c.Convey("The next page body should contain \"Click here to Contribute\"", func() {
 			r.ServeHTTP(record, req)
 			c.So(record.Code, c.ShouldEqual, http.StatusOK)
-			c.So(fmt.Sprint(record.Body), c.ShouldContainSubstring, `I am an alcoholic`)
+			c.So(fmt.Sprint(record.Body), c.ShouldContainSubstring, `Click here to Contribute`)
 		})
 	})
 }
@@ -48,18 +51,24 @@ func TestPostHomePage(t *testing.T) {
 	ctx, inst := testers.GetTestingContext()
 	defer inst.Close()
 
-	c.Convey("When you post at the root URL", t, func() {
+	c.Convey("When you register with a valid email address", t, func() {
 		r := createHTTPRouter(handlers.ToHTTPHandlerConverter(ctx))
-		record := httptest.NewRecorder()
+		record2 := httptest.NewRecorder()
 
-		req, err := http.NewRequest("POST", "/", nil)
+		formData2 := url.Values{}
+		formData2.Set("stripeEmail", "member@meeting.attendee.glom")
+		formData2.Set("stripeToken", "tok_visa")
+
+		req2, err := http.NewRequest("POST", "/charge", strings.NewReader(formData2.Encode()))
+		req2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		req2.Header.Add("Content-Length", strconv.Itoa(len(formData2.Encode())))
 
 		c.So(err, c.ShouldBeNil)
 
-		c.Convey("The next page body should contain \"Charge successful\"", func() {
-			r.ServeHTTP(record, req)
-			c.So(record.Code, c.ShouldEqual, http.StatusOK)
-			c.So(fmt.Sprint(record.Body), c.ShouldContainSubstring, `Charge successful`)
+		c.Convey("The next page body should contain \"Payment successful\"", func() {
+			r.ServeHTTP(record2, req2)
+			c.So(fmt.Sprint(record2.Body), c.ShouldContainSubstring, `Payment successful`)
+			c.So(record2.Code, c.ShouldEqual, http.StatusOK)
 		})
 	})
 }
